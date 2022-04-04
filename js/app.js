152f6ae6f0a070e065e9311c96b716b921398c8c
Jack <Jack011601@gmail.com>
2022-04-04 12:16:57 -0500

    added directions for project in app.js

---
+++ js/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*
+ Problem Domain --> calculate number of cookies by location every day
+  
+
+
+
+1. create objects for each store.
+    - attributes (given to us in description)
+        - min/max hourly customers
+        - avg cookies per customer
+
+2. Use a method with that object to generate a random number.
+3. Calculate and store amount of cookies purchased each hour at each
+    location using avg cookies purchased and random number of customers generated.
+
+4. Store results for each location in a separate array. 
+    - property of the object representing that location
+
+5. Display the values of each array as a <ul> on the browser
+
+6. Calculate the sum of hourly totals for each location.
+
+
+*/
